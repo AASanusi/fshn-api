9d0ada66128ad4554f4af247fe95fff483d4ccaf
AASanusi <alaminm.sanusi@gmail.com>
2023-03-11 22:43:06 +0000

    Add secret key correctly in settings.py

--- fshn_api/settings.py
+++ fshn_api/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e9_w*p1%$g%bywlb3hrhbc98e6jkwfk(9mw9r)zv55h_)2#$an'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
