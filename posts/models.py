04f519e2a758f147e902283e91c3eee6c5150ddb
AASanusi <alaminm.sanusi@gmail.com>
2023-03-12 17:12:18 +0000

    Add image_filter choice field and image validation

--- posts/models.py
+++ posts/models.py
@@ -17,6 +17,25 @@ class Post(models.Model):
     image = models.ImageField(
         upload_to='images/', default='../default_post_xzufhn', blank=True
     )
+    image_filter_choices = [
+        ('_1977', '1977'),
+        ('brannan', 'Brannan'),
+        ('earlybird', 'Earlybird'),
+        ('hudson', 'Hudson'),
+        ('inkwell', 'Inkwell'),
+        ('lofi', 'Lo-Fi'),
+        ('kelvin', 'Kelvin'),
+        ('normal', 'Normal'),
+        ('nashville', 'Nashville'),
+        ('rise', 'Rise'),
+        ('toaster', 'Toaster'),
+        ('valencia', 'Valencia'),
+        ('walden', 'Walden'),
+        ('xpro2', 'X-pro II')
+    ]
+    image_filter = models.CharField(
+        max_length=32, choices=image_filter_choices, default='normal'
+    )
 
     class Meta:
         ordering = ['-created_at']
---
+++ posts/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+from posts.models import Post
+
+# Parts taken from DRF-API walkthrough.
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source='owner.profile.id')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+
+    def validate_image(self, value):
+        """
+        Validation checks on size, height and width on post images
+        """
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError(
+                'Image 2MB file size limit')
+
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                'Image height larger than 4096px!'
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                'Image width needs to be smaller than 4096px!'
+            )
+        return value
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+
+    class Meta:
+        model = Post
+        fields = [
+            'id', 'owner', 'is_owner', 'profile_id',
+            'profile_image', 'created_at', 'updated_at',
+            'title', 'content', 'image', 'image_filter'
+        ]
